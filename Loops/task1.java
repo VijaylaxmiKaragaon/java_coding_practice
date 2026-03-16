829205be39708259a07153d5bf579799703a11fe
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-03-17 00:11:28 +0530

    added loops folder

---
+++ Loops/task1.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class task1 {
+    public static void main(String args[]){
+        int counter = 1;
+        while(counter<= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
---
+++ Loops/task5.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class task5 {
+    public static void main(String args[]){
+        // for(int i=1;i<=4;i++){
+        //     System.out.println("****");
+        // }
+
+        int line=1;
+        while(line<= 4){
+            System.out.println("****");
+            line++;
+        }
+    }
+}
+
+//output
+// ****
+// ****
+// ****
+// ****
