32854b65473a717b5fece044e62326baf2128738
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-02 22:24:52 +0530

    added folder

---
+++ Operators/AirthmeticOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class AirthmeticOperator {
+    public static void main(String[] args) {
+		//System.out.println(101+201); //302
+		//System.out.println(101-201); //-100
+//	int a = 10;
+//		int b = 3;
+		//System.out.println(a/b); //3
+//		float c = a/b;
+		//System.out.println(c); //3.0
+		//System.out.println(a%b); //1
+		
+		int a = 3;
+		int b = 5;
+		System.out.println(a%b); //3
+	}
+}
