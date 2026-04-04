2f476fce61cda7b77a60773ef9cf8fb67c586947
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-04 23:38:38 +0530

    added folder

---
+++ Loops/ForLoop.java
@@ -0,0 +1,29 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+//		for(int i=1;i<=5;i++) {
+//			System.out.println("Hi");
+//			output:
+//				Hi
+//				Hi
+//				Hi
+//				Hi
+//				Hi
+			
+//			for(int i=1; ;i++) {
+//				System.out.println("Hi"); //infinite loop
+		    
+//		for(int i=1; ;) {
+//			System.out.println("Hi"); //infinite loop
+		
+//		for(; ;) {
+//		System.out.println("Hi"); //indinite loop
+		
+		
+		for(int i=0;i<=5;i++) {
+			System.out.println(i); // 0 1 2 3 4 5
+		
+		}
+    }
+}
