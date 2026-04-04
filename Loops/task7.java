aa098a7e4df624dfd64d0ea7ba48740be5379cf9
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-04 23:40:15 +0530

    added folder

---
+++ Loops/task7.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class task7 {
+    public static void main(String[] args) {
+		int marks = 90;
+		if(marks >= 85) {
+			System.out.println("distinction");
+		} else if(marks >= 60) {
+			System.out.println("first class");
+		} else if(marks >= 45) {
+			System.out.println("second class");
+		} else if(marks >= 35) {
+			System.out.println("just pass");
+		} else {
+			System.out.println("just fail");
+		}
+
+	}
+}
