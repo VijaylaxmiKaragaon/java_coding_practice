9f8aae3f30c90ffba2abafa5be7a0b305ccb4743
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-11 22:39:17 +0530

    added program files

---
+++ patterns/Prgm28.java
@@ -0,0 +1,30 @@
+package patterns;
+
+public class Prgm28 {
+    public static void main(String[] args) {
+		int rows = 5;
+		for(int i=1;i<=rows;i++) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=rows+1-i;j<=rows;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			for(int j=rows-1;j>=rows+1-i;j--) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+//output
+//
+//
+//        E 
+//      D E D 
+//    C D E D C 
+//  B C D E D C B 
+//A B C D E D C B A 
