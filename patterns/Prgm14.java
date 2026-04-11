3f57677cd8a624904bdc7b101bc8c2486208967e
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-11 22:37:05 +0530

    added program files

---
+++ patterns/Prgm14.java
@@ -0,0 +1,26 @@
+package patterns;
+
+public class Prgm14 {
+    public static void main(String[] args) {
+	 int rows = 5;
+	 for(int i=1;i<=rows;i++) {
+		 for(int j=1;j<=rows-i;j++) {
+			 System.out.print(" "+" ");
+		 }
+		 for(int j=i;j>=1;j--) {
+			 System.out.print(j+" ");
+		 }
+		 System.out.println();
+	 }
+
+	}
+
+}
+
+//output
+//
+//        1 
+//      2 1 
+//    3 2 1 
+//  4 3 2 1 
+//5 4 3 2 1 
---
+++ patterns/Prgm16.java
@@ -0,0 +1,26 @@
+package patterns;
+
+public class Prgm16 {
+    public static void main(String[] args) {
+		int rows = 5;
+		for(int i=1;i<=rows;i++) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=rows;j>=rows+1-i;j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+//output
+//
+//        5 
+//      5 4 
+//    5 4 3 
+//  5 4 3 2 
+//5 4 3 2 1 
