908900b869b35bc40f5aa3233d370f7faa3244ac
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-11 22:41:28 +0530

    added program files

---
+++ patterns/Prgm44.java
@@ -0,0 +1,46 @@
+package patterns;
+
+public class Prgm44 {
+    public static void main(String[] args) {
+		int rows =5;
+		for(int i=rows;i>=1;i--) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=i;j>=1;j--) {
+				System.out.print((char)(j+64)+" ");
+			}
+			for(int j=2;j<=i;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=2;i<=rows;i++) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=i;j>=1;j--) {
+				System.out.print((char)(j+64)+" ");
+			}
+			for(int j=2;j<=i;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+//output
+//
+//E D C B A B C D E 
+//  D C B A B C D 
+//    C B A B C 
+//      B A B 
+//        A 
+//      B A B 
+//    C B A B C 
+//  D C B A B C D 
+//E D C B A B C D E 
---
+++ patterns/Prgm45.java
@@ -0,0 +1,46 @@
+package patterns;
+
+public class Prgm45 {
+    public static void main(String[] args) {
+		int rows = 5;
+		for(int i=rows;i>=1;i--) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=rows;j>=rows+1-i;j--) {
+				System.out.print((char)(j+64)+" ");
+			}
+			for(int j=rows+2-i;j<=rows;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=2;i<=rows;i++) {
+			for(int j=1;j<=rows-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int j=rows;j>=rows+1-i;j--) {
+				System.out.print((char)(j+64)+" ");
+			}
+			for(int j=rows+2-i;j<=rows;j++) {
+				System.out.print((char)(j+64)+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+//output
+//
+//E D C B A B C D E 
+//  E D C B C D E 
+//    E D C D E 
+//      E D E 
+//        E 
+//      E D E 
+//    E D C D E 
+//  E D C B C D E 
+//E D C B A B C D E 
