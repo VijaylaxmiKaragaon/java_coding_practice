44603798e072b08b6a8042f479c4ffcc22243fe1
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-04-13 22:06:09 +0530

    added program

---
+++ patterns/Prgm51.java
@@ -0,0 +1,24 @@
+package patterns;
+
+public class Prgm51 {
+    public static void main(String[] args) {
+		int rows = 5;
+		for(int i=1;i<=rows;i++) {
+			int k = rows+1-i;
+			for(int j=1;j<=i;j++) {
+				System.out.print(k+" ");
+				k++;
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+//output
+//
+//5 
+//4 5 
+//3 4 5 
+//2 3 4 5 
+//1 2 3 4 5 
