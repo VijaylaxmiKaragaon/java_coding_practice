65a9518c6a1c799015a2c46e095f8d642f93f0ac
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-05-30 22:15:16 +0530

    add program file

---
+++ Arrays/TotalPairsInAnArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class TotalPairsInAnArray {
+
+	public static void printPairs(int numbers[]) {
+		int tp=0;
+		for(int i=0;i<numbers.length;i++) {
+			int curr=numbers[i];
+			for(int j=i+1;j<numbers.length;j++) {
+				System.out.print("("+curr+","+numbers[j]+")");
+				tp++;
+			}
+			System.out.println("Total pairs = "+tp);
+		}
+	}
+	public static void main(String[] args) {
+		int numbers[]= {2,3,4,6,8,9};
+		printPairs(numbers);
+
+	}
+
+}
