f21f96e9c501f7fd84c64d45f96211bfd11221f7
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-05-31 13:27:33 +0530

    add program file

---
+++ Arrays/PMaxSubArraySum.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class PMaxSubArraySum{
+    public static void maxSubArraySum(int numbers[]) {
+    	int maxSum=	Integer.MIN_VALUE;
+    	int currSum=0;
+    	int prefix[] = new int[numbers.length];
+    	
+    	prefix[0] = numbers[0];
+    	//calculate prefix array
+    	for(int i=1; i<prefix.length;i++) {
+    		prefix[i] = prefix[i-1] + numbers[i];
+    	}
+    	for(int i=0;i<numbers.length;i++) {
+    		int start = i;
+    		for(int j=i;j<numbers.length;j++) {
+    			int end = j;
+    			
+    			currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start];
+    			if(maxSum < currSum) {
+    				maxSum = currSum;
+    			}
+    		}
+    	}
+    	System.out.println("max sum = "+ maxSum);
+    }
+	public static void main(String[] args) {
+		int[] numbers= {1,-2,6,-1,3};
+		maxSubArraySum(numbers);
+	}
+
+}
+
